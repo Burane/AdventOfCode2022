9fe4015c2fac6b149036944b50ba31f747dd6444
Antoine <kerbiriouantoine@gmail.com>
2022-12-02 00:59:08 +0100

    day one part two

--- src/days/day1/mod.rs
+++ src/days/day1/mod.rs
@@ -6,6 +6,11 @@ use super::get_input;
  * https://adventofcode.com/2022/day/1
  */
 pub fn solve() {
+    part_one();
+    part_two();
+}
+
+fn part_one() {
     let input = get_input(1);
     let soluce = input
         .split("\r\n\r\n")
@@ -20,5 +25,27 @@ pub fn solve() {
     println!(
         "The elf carrying the most calories is carrying a total of {} calories",
         soluce.unwrap()
+    );
+}
+
+fn part_two() {
+    let input = get_input(1);
+    let mut sums: Vec<isize> = input
+        .split("\r\n\r\n")
+        .map(|split| {
+            split
+                .lines()
+                .map(|s| s.parse::<isize>().unwrap())
+                .sum::<isize>()
+        })
+        .collect();
+
+    sums.sort();
+    sums.reverse();
+    let soluce = &sums.as_slice()[0..3].iter().sum::<isize>();
+
+    println!(
+        "The 3 elf carrying the most calories are carrying a total of {} calories",
+        soluce
     );
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod days;
 
 use days::day1;
 
-/** 
+/**
  * Advent of Code 2022 written in rust
  * https://adventofcode.com/2022
  */
